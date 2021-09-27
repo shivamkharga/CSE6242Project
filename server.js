c32e0e32eb079a4b477a81a6079c6177d1b7f7f1
Shivam Kharga <shivam.kharga@hotmail.com>
2021-09-27 15:59:59 -0600

    server setup

--- server.js
+++ server.js
@@ -9,21 +9,28 @@ const users = require("./backend/routes/api/users");
 
 app.set('port',port);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname,"dist")));
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname,"dist")));
   
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "dist/myapp/index.html"));
-    });
-  } else {
-    express.static("static");
+//     app.get("*", (req, res) => {
+//       res.sendFile(path.join(__dirname, "dist/myapp/index.html"));
+//     });
+//   } else {
+//     express.static("static");
   
-    app.get("/", function(req, res) {
-      res.send("hello from expressjs");
-    });
-  }
-app.use("/users", users);
+//     app.get("/", function(req, res) {
+//       res.send("hello from expressjs");
+//     });
+//   }
 
+app.use(express.static(path.join(__dirname,"dist")));
+  
+app.get("*", (req, res) => {
+    console.log("here");
+    res.sendFile(path.join(__dirname, "dist/myapp/index.html"));
+});
+
+app.use("/users", users);
 app.listen(port, (req, res) => {
     console.log("listening to port: " + port);
   });
