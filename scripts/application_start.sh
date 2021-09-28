e667cf357cb090196dba6b696aff2f9af4e379d8
Shivam Kharga <shivam.kharga@hotmail.com>
2021-09-27 22:00:50 -0600

    Added static config

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -15,4 +15,4 @@ export NVM_DIR="$HOME/.nvm"
 npm install
 
 #start our node app in the background
-node server.js > app.out.log 2> app.err.log < /dev/null & 
+npm start > app.out.log 2> app.err.log < /dev/null & 
