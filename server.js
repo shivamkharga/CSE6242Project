7af416eea05e666e0f894f14bb1d7065b1c57034
Shivam Kharga <shivam.kharga@hotmail.com>
2021-09-27 22:13:11 -0600

    Added static config

--- server.js
+++ server.js
@@ -5,15 +5,24 @@ const app=express();
 
 const port = process.env.PORT || 9000;
 
+app.set('port',port);
 const users = require("./backend/routes/api/users");
 
-app.set('port',port);
 app.use("/api/users", users);
-app.use('/',express.static(path.join(__dirname,"backend","angular")));
-  
-app.use((req,res,next)=>{
-    res.sendFile(path.join(__dirname,"backend","angular","index.html"));
-});
+
+if (process.env.NODE_ENV === "production") {
+    
+    app.use('/',express.static(path.join(__dirname,"backend","angular")));
+
+    app.use((req,res,next)=>{
+        res.sendFile(path.join(__dirname,"backend","angular","index.html"));
+    });
+}
+else{
+    app.get("/", function(req, res) {
+        res.send("hello from expressjs");
+      });
+}
 
 app.listen(port, (req, res) => {
     console.log("listening to port: " + port);
