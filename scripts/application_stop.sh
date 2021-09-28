6c45c290797a25c7e9fc5014a8f7bebf1b66731e
Shivam Kharga <shivam.kharga@hotmail.com>
2021-09-27 23:08:18 -0600

    Added static config

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-#Stopping existing node servers
-echo "Stopping any existing node servers"
-pkill npm
+. $(dirname $0)/constants.sh
+service $application_name-$environment stop
+#Stop the server application
+
+#Force kill server if something is still running on its port.
+pid=lsof -t -i:$server_port
+if [ "$pid" != "" ]
+then # Kill the running process
+kill -9 $pid 2>/dev/null || :
+fi
+
+#To remove the current PID file
+if [ -e $pid_path ]
+then
+test -x $DAEMON || exit 0
+set -e
