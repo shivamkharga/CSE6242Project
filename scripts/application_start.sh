3e7a68531d7b544d4b5ddd9dec08ff27c534f83e
Shivam Kharga <shivam.kharga@hotmail.com>
2021-10-20 09:19:42 -0600

    Changed node version

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,6 +12,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
+nvm install v16.10.0
+npm install -g yarn
+yarn add vue-cli
 npm install
 yarn install
 
