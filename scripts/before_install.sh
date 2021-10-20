16e9cbdf91ed72d7878807c37b75304f07746ef4
Shivam Kharga <shivam.kharga@hotmail.com>
2021-10-20 08:15:03 -0600

    changes in cicd script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,9 +2,11 @@
 
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
 . ~/.nvm/nvm.sh
 
 nvm install node
+
 npm install -g yarn
 yarn add vue-cli
 
