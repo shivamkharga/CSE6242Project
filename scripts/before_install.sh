8bfc77d151ba1429bf11d6498a703a0e0944fc45
Shivam Kharga <shivam.kharga@hotmail.com>
2021-10-20 00:30:07 -0600

    changes in cic script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,19 +2,17 @@
 
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.
+. ~/.nvm/nvm.sh
 
 nvm install node
-
 npm install -g yarn
-
 yarn add vue-cli
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/CSE6242PROJECT"
 if [ -d "$DIR" ]; then
-  sudo rm -rf ${DIR}
-
-echo "Creating ${DIR} directory"
-mkdir ${DIR}
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
 fi
