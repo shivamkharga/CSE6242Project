39abe3748f2e838f651b1b293adbdb2aac33ae1e
Shivam Kharga <shivam.kharga@hotmail.com>
2021-11-19 20:13:12 -0700

    Added package installer for python

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,6 +2,9 @@
 
 #give permission for everything in the express-app directory
 sudo chmod -R 777 /home/ec2-user/CSE6242PROJECT
+sudo apt-get update
+sudo apt install python-pip
+
 
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/CSE6242PROJECT
@@ -12,6 +15,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
+
+pip install numpy
+pip install pandas
+pip install -U scikit-learn
 nvm install v16.10.0
 npm install -g yarn
 yarn add vue-cli
