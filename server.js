6dd6cd240ce59f0ee4a25543d13db48668f7f75d
Shivam Kharga <shivam.kharga@hotmail.com>
2021-11-20 06:41:10 -0700

    removed env variable

--- server.js
+++ server.js
@@ -7,23 +7,13 @@ const port = process.env.PORT || 80
 app.set('port', port)
 const users = require('./backend/routes/api/users')
 
-console.log('env:', process.env.NODE_ENV)
-
 app.use('/api/users', users)
 
-if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, 'dist')))
+app.use('/', express.static(path.join(__dirname, 'dist')))
 
-  app.use((req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-  })
-} else {
-  express.static('static')
-
-  app.get('/', function (req, res) {
-    res.send('hello from nodejs. Frontend is at different port ')
-  })
-}
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 app.listen(port, () => {
   console.log(`listening to port: ${port}`)
