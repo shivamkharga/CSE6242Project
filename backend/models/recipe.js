99206e85d68a3b8ce4f915a9f4d4c834d731bba8
Shivam Kharga <shivam.kharga@hotmail.com>
2021-11-28 12:42:01 -0700

    Added description in recipe collection. changed name of collection

--- backend/models/recipe.js
+++ backend/models/recipe.js
@@ -10,7 +10,7 @@ const recipeSchema = new Schema(
     //   steps: String,
     avg_ratings: Number,
   },
-  { collection: 'receipe' }
+  { collection: 'recipe' }
 )
 
 module.exports = mongoose.model('recipe', recipeSchema)
